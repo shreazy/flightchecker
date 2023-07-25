f6598d0974bf322c8c1d6863cba9aea970ae248f
sharoz <sharoz.ahmad@gmx.de>
2023-07-25 06:41:04 +0200

    Project Setup

---
+++ backend/src/test/java/de/neuefische/sharoz/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.sharoz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
